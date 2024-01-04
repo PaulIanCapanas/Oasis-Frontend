d83aa2a89922331b8c96608d4a5db428e5481966
Aljason Javier <89983365+AljasonJavier@users.noreply.github.com>
2024-01-04 17:58:52 +0800

    set-api-singup

--- src/components/SignUpPage.tsx
+++ src/components/SignUpPage.tsx
@@ -4,7 +4,10 @@ import "../App.css";
 
 const SignupPage = () => {
   const [formData, setFormData] = useState({
-    username: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    age: '',
     password: '',
   });
 
@@ -18,9 +21,10 @@ const SignupPage = () => {
 
   const handleSubmit = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/create-user', formData);
+      const response = await axios.post('http://localhost:3000/user/register', formData);
       console.log('Server response:', response.data);
       console.log('SignUp succesful!')
+      
     } catch (error: any) {
     console.error('Error submitting form:', error);
     console.log('Axios response:', error.response);
@@ -38,28 +42,56 @@ const SignupPage = () => {
           <div className="input">
             <input
               type="text"
-              placeholder="Username"
-              name="username"
-              value={formData.username}
+              placeholder="first_name"
+              name="first_name"
+              value={formData.first_name}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="input">
+            <input
+              type="text"
+              placeholder="last_name"
+              name="last_name"
+              value={formData.last_name}
               onChange={handleChange}
             />
           </div>
           <div className="input">
             <input
-              type="password"
-              placeholder="Password"
+              type="text"
+              placeholder="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="age">
+            <input
+              type="Number"
+              placeholder="age"
+              name="age"
+              value={formData.age}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="password">
+            <input
+              type="text"
+              placeholder="password"
               name="password"
               value={formData.password}
               onChange={handleChange}
             />
           </div>
         </div>
-        <div className="texts">Already have an account?<a href="/login">Log in here</a></div>
         <div className="login">
           <button className="submit" onClick={handleSubmit}>Sign Up</button>
         </div>
       </div>
-    </div>
+    
+    
   );
 };
 
