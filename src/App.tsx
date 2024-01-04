8fc568abd7e0402cdfa1414557af2f8ce688e6bf
gianCristianBaguna <102247108+gianCristianBaguna@users.noreply.github.com>
2024-01-05 03:33:15 +0800

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/customer-home-page" element={<CustomerHomePage/>} />
         <Route path="/results" element={<ResultsPage/>}/>
         <Route path="/user-type" element={<UserType/>} />
-        <Route path="/description" element={<Description/>}/>
+        <Route path="/description" element={<Description/>} />
         {/* <Route path="/user-page" element={<UserPage/>} />
         <Route path="/chat" element={<ChatPage/>} /> */}
       </Routes>
