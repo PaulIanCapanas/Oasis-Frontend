ac255794da6bb4b396c108526beb474aa40025b3
gianCristianBaguna <102247108+gianCristianBaguna@users.noreply.github.com>
2024-01-05 03:25:57 +0800

    added pages amd UI

--- src/components/CustomerHomeNav.tsx
+++ src/components/CustomerHomeNav.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import MenuBar from "../assets/menubar.png";
+import Logo from "../assets/logo.png"
 
 const CustomerHomeNav: React.FC = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
@@ -11,7 +12,10 @@ const CustomerHomeNav: React.FC = () => {
 
   return (
     <nav className="fixed w-full flex items-center justify-between p-4 bg-gray-800">
-      <Link to="/homePage" className="text-2xl font-bold text-white hover:text-gray-300">
+      <div className="pr-4">
+        <img src={Logo} alt="" className="w-15 h-12 cursor-pointer"/>
+      </div>
+      <Link to="/customer-home-page" className="text-2xl font-bold text-white hover:text-gray-300">
         Oasis
       </Link>
       <div className="flex items-center space-x-4 ml-auto">
@@ -19,7 +23,7 @@ const CustomerHomeNav: React.FC = () => {
           <img
             src={MenuBar}
             alt="Menu"
-            className="w-6 h-6 cursor-pointer"
+            className="w-12 h-12 cursor-pointer"
             onClick={toggleDropdown}
           />
           <div
